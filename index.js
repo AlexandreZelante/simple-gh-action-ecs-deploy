ffcae8500b4ea63127328112222c50898d07d799
Alexandre Zelante <alexandre.zelante@outlook.com>
2023-11-04 17:27:24 -0300

    change hello world to 3

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function (req, res){
-    res.send("Hello World 2");
+app.get('/', function (req, res) {
+    res.send("Hello World 3!");
 });
 
-app.listen(80);
+app.listen(80, () => {
+    console.log('Rodando demais.');
+});
